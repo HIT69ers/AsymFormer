a035dbe67d3eaa02eeab232e9dfbda61f70390f0
HIT69ers <2022113442@stu.hit.edu.cn>
2026-01-15 14:37:38 +0800

    B0_S

--- biformer_eval.py
+++ biformer_eval.py
@@ -16,8 +16,8 @@ from src.biformer import biformer
 from utils import utils
 from utils.utils import load_ckpt, intersectionAndUnion, AverageMeter, accuracy, macc
 
-pth_dir = "/mnt/syh/asym_checkpoints/Biformer_b3_b0_0.5_M1_bsize8/ckpt_epoch_500.00.pth"
-model = biformer(num_classes=40, downsample_ratio=0.5)
+pth_dir = "/mnt/syh/asym_checkpoints/Biformer_b3_b0_1.0_M1_bsize8/ckpt_epoch_175.00.pth"
+model = biformer(num_classes=40, downsample_ratio=1.0)
 
 parser = argparse.ArgumentParser(description='RGBD Sementic Segmentation')
 parser.add_argument('--data-dir', default="/mnt/syh/datasets/NYUv2/data/", metavar='DIR',
--- src/convnext.py
+++ src/convnext.py
@@ -165,11 +165,29 @@ def convnext_tiny(pretrained=False,in_22k=False, **kwargs):
     return model
 
 @register_model
+def convnext_tiny_local(pretrained=False, in_22k=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768], **kwargs)
+    if pretrained:
+        # url = model_urls['convnext_tiny_22k'] if in_22k else model_urls['convnext_tiny_1k']
+        checkpoint = torch.load(pretrained, map_location="cpu")
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+@register_model
 def convnext_small(pretrained=False,in_22k=False, **kwargs):
     model = ConvNeXt(depths=[3, 3, 27, 3], dims=[96, 192, 384, 768], **kwargs)
     if pretrained:
         url = model_urls['convnext_small_22k'] if in_22k else model_urls['convnext_small_1k']
         checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu")
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+@register_model
+def convnext_small_local(pretrained=False, in_22k=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 27, 3], dims=[96, 192, 384, 768], **kwargs)
+    if pretrained:
+        # url = model_urls['convnext_tiny_22k'] if in_22k else model_urls['convnext_tiny_1k']
+        checkpoint = torch.load(pretrained, map_location="cpu")
         model.load_state_dict(checkpoint["model"])
     return model
 
